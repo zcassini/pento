d6ebd75e573f80d985b92458fccb83a894609851
zcassini <zcassini@gmail.com>
2022-03-24 08:25:35 -0400

    initial commit

---
+++ lib/pento_web/live/product_live/form_component.ex
@@ -0,0 +1,62 @@
+defmodule PentoWeb.ProductLive.FormComponent do
+  use PentoWeb, :live_component
+
+  alias Pento.Catalog
+
+  @impl true
+  def update(%{product: product} = assigns, socket) do
+    changeset = Catalog.change_product(product)
+    Process.sleep(250)
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign(:changeset, changeset)
+     |> allow_upload(:image,
+     accept: ~w(.jpg .jpeg .png),
+     max_entries: 1,
+     max_file_size: (9_000_000),
+     auto_upload: true,
+     # progress: &handle_progress/3
+     )}
+  end
+
+  @impl true
+  def handle_event("validate", %{"product" => product_params}, socket) do
+    changeset =
+      socket.assigns.product
+      |> Catalog.change_product(product_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, :changeset, changeset)}
+  end
+
+  def handle_event("save", %{"product" => product_params}, socket) do
+    save_product(socket, socket.assigns.action, product_params)
+  end
+
+  defp save_product(socket, :edit, product_params) do
+    case Catalog.update_product(socket.assigns.product, product_params) do
+      {:ok, _product} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Product updated successfully")
+         |> push_redirect(to: socket.assigns.return_to)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, :changeset, changeset)}
+    end
+  end
+
+  defp save_product(socket, :new, product_params) do
+    case Catalog.create_product(product_params) do
+      {:ok, _product} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Product created successfully")
+         |> push_redirect(to: socket.assigns.return_to)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
+  end
+end
--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -1,6 +1,8 @@
 defmodule PentoWeb.Router do
   use PentoWeb, :router
 
+  import PentoWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule PentoWeb.Router do
     plug :put_root_layout, {PentoWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -18,7 +21,6 @@ defmodule PentoWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
-    live "/guess", WrongLive
   end
 
   # Other scopes may use custom stacks.
@@ -53,5 +55,53 @@ defmodule PentoWeb.Router do
 
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", PentoWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    get "/users/register", UserRegistrationController, :new
+    post "/users/register", UserRegistrationController, :create
+    get "/users/log_in", UserSessionController, :new
+    post "/users/log_in", UserSessionController, :create
+    get "/users/reset_password", UserResetPasswordController, :new
+    post "/users/reset_password", UserResetPasswordController, :create
+    get "/users/reset_password/:token", UserResetPasswordController, :edit
+    put "/users/reset_password/:token", UserResetPasswordController, :update
+  end
+
+  scope "/", PentoWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    get "/users/settings", UserSettingsController, :edit
+    put "/users/settings", UserSettingsController, :update
+    get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+    live "/guess", WrongLive
+    live "/promo", PromoLive
+
+    live "/products", ProductLive.Index, :index
+    live "/products/new", ProductLive.Index, :new
+    live "/products/:id/edit", ProductLive.Index, :edit
+
+    live "/products/:id", ProductLive.Show, :show
+    live "/products/:id/show/edit", ProductLive.Show, :edit
+  end
+
+  scope "/", PentoWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+    get "/users/confirm", UserConfirmationController, :new
+    post "/users/confirm", UserConfirmationController, :create
+    get "/users/confirm/:token", UserConfirmationController, :edit
+    live "/products", ProductLive.Index, :index
+    live "/products/new", ProductLive.Index, :new
+    live "/products/:id/edit", ProductLive.Index, :edit
+
+    live "/products/:id", ProductLive.Show, :show
+    live "/products/:id/show/edit", ProductLive.Show, :edit
+    post "/users/confirm/:token", UserConfirmationController, :update
   end
 end
